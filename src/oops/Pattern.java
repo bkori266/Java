1555534e8c4b3d80a5d9d94fc117019380747a3c
Bipin Kori <83345282+bkori266@users.noreply.github.com>
2026-01-28 15:59:00 +0530

    fact added

--- src/oops/Pattern.java
+++ src/oops/Pattern.java
@@ -10,9 +10,7 @@ public class Pattern {
 		Pattern p1=new Pattern();
 		System.out.println("Enter Box size");
 //		p1.triangle(sc.nextInt());
-		
-		
-
+		p1.fact(4);
 	}
 
 	private void triangle(int size) {
@@ -32,13 +30,22 @@ public class Pattern {
 		}
 		
 	}
-private void rectangle(int size) {
+	private void rectangle(int size) {
 		for(int i=0;i<size;i++) {
 			for(int j=0;j<size;j++)
 			{System.out.print("*");}
 			System.out.println();
 		}
 		
+	}
+
+	public void fact(int number) {
+		long ans=1;
+		for (int i=1;i<=number;i++) {
+			ans=i*ans;
+		}
+		
+		System.out.println(ans);
 	}
 
 	
